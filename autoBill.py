496a1efcbe7ad3ad068563b678269195e5486648
xsin6208 <xsin6208@uni.sydney.edu.au>
2026-02-05 17:43:00 +1100

    Refactor patient processing logic in autoBill.py
    Refactor patient processing functions and remove unused code.

--- autoBill.py
+++ autoBill.py
@@ -2,20 +2,18 @@ from pathlib import Path
 from fileToText import extract_data
 from billProcessor import processPatient
 from configureButtons import configure, ButtonPress
+from helperFunctions import load_json, save_json, get_window_name
 import keyboard
 import json
 import string
 import re
 import pyautogui
-import win32gui
-from pywinauto import Desktop
 import time
 import winsound
 import builtins
 
 skip = False
 
-# Save originals
 _original_print = builtins.print
 _original_input = builtins.input
 
@@ -39,30 +37,7 @@ def printAllDetails():
 
 keyboard.add_hotkey("alt+ctrl+p", printAllDetails, suppress=True)
 
-def load_json(path):
-    if not path.exists():
-        return {}
-
-    with path.open("r", encoding="utf-8") as f:
-        return json.load(f)
-    
-def save_json(path, data):
-    with path.open("w", encoding="utf-8") as f:
-        json.dump(data, f, indent=2)
-
-def get_window_name():
-        hwnd = win32gui.GetForegroundWindow()
-        desktop = Desktop(backend="uia")
-
-        # Get the foreground window by handle
-        active_window = desktop.window(handle=hwnd)
-        return active_window.window_text()
-
-def correctDetails(patient, buttons):
-    global collectionMode
-    time.sleep(0.5)
-    if get_window_name() != "Windows PowerShell":
-        buttons["Terminal"].clickButton()
+def ensurePatientFound(patient, buttons):
     if "PatientName" not in patient:
         if "PatientDOB" in patient:
             print(patient["PatientDOB"])
@@ -71,7 +46,7 @@ def correctDetails(patient, buttons):
             print(patient, "Couldn't find any patient associated to this information")
             return 1
     
-    
+def processPatientQuery(patient, buttons):
     print(f"Patient: {patient["PatientName"]}")
     if "Times" in patient:
         print("Patient has high chance of having both 55118 and 61109")
@@ -84,8 +59,10 @@ def correctDetails(patient, buttons):
     while inp != "n" and inp != "y":
         inp = input("Do you want to process patient? y/n")
     if inp == "n":
-        return 1
-    
+        return False
+    return True
+
+def checkDOBandHospital(patient, buttons):
     if "PatientDOB" not in patient:
         patient["PatientDOB"] = input(f"Please put in DOB dd/mm/yyyy for {patient["PatientName"]}")
 
@@ -103,9 +80,11 @@ def correctDetails(patient, buttons):
                 buttons["Terminal"].clickButton()
             hosp = input("Hospital couldn't be found, type 'skip' to skip or provide hospital acronym in caps")
             if hosp == "skip":
-                return 1
+                return False
             patient["Hosp"] = hosp
-    
+    return True
+
+def ensureCorrectDate(patient, buttons):
     if "DerivedDate" in patient:
         if "DateSecondary" in patient:
             dateSec = re.split(r"[\/\.\-\s]+", patient["DateSecondary"])
@@ -131,7 +110,63 @@ def correctDetails(patient, buttons):
                 patient["ProcDate"] = patient["DateSecondary"] 
             else:
                 patient["ProcDate"] = inp
+
+def correctAndSaveNumbers(patient, buttons, items_db, number, i=-1):
+    number = number.translate(str.maketrans("", "", string.punctuation))
+    number = re.sub(r"\D", "", number)
+    if number in items_db:
+        if items_db[number] != number:
+            inp = input(f"Is {items_db[number]} supposed to be instead of {number}? Enter y or enter the true number")
+            if inp == "y":
+                if i == -1:
+                    patient["ProcNumbers"] = items_db[number]
+                else:
+                    patient["ProcNumbers"][i] = items_db[number]
+            else:
+                if i == -1:
+                    patient["ProcNumbers"] = inp
+                else:
+                    patient["ProcNumbers"][i] = inp
+        else:
+            if i == -1:
+                patient["ProcNumbers"] = number
+            else:
+                patient["ProcNumbers"][i] = number
+    else:
+        inp = input(f"Enter 'y' if {number} is correct, if not enter true number")
+        if inp == "y":
+            items_db[number] = number
+            if i == -1:
+                patient["ProcNumbers"] = number
+            else:
+                patient["ProcNumbers"][i] = number
     
+        else:
+            if i == -1:
+                patient["ProcNumbers"] = inp
+            else:
+                patient["ProcNumbers"][i] = inp
+            items_db[number] = inp
+
+def ensureCorrectItemNumbers(patient, buttons):
+    items_json = Path("itemNums.json")
+    items_db = load_json(items_json)
+    if "ProcNumbers" not in patient:
+        patient["ProcNumbers"] = list()
+        inp = input("Enter in numbers until all in, then enter y")
+        while inp != "y":
+            patient["ProcNumbers"].append(inp)
+            if inp not in items_db:
+                items_db[inp] = inp
+    elif isinstance(patient["ProcNumbers"], list):
+        for i, number in enumerate(patient["ProcNumbers"]):
+            correctAndSaveNumbers(patient, buttons, items_db, number, i)
+    else:
+        number = patient["ProcNumbers"]
+        correctAndSaveNumbers(patient, buttons, items_db, number)
+    save_json(items_json, items_db)
+
+def ensureCorrectReferral(patient, buttons):
     referral_json = Path("ref.json")
     referral_db = load_json(referral_json)
     if "DerivedReferral" not in patient or patient["DerivedReferral"] == "HT":
@@ -170,7 +205,7 @@ def correctDetails(patient, buttons):
     else:
         if patient["DerivedReferral"] in referral_db:
             patient["Referral"] = patient["DerivedReferral"]
-        elif collectionMode:
+        else:
             inp = input(f"Is {patient["DerivedReferral"]} correct? y/n")
             if inp == "y":
                 referral_db[patient["DerivedReferral"]] = "-1"
@@ -178,128 +213,64 @@ def correctDetails(patient, buttons):
             else:
                 inp = input("Please paste in correct referral")
                 patient["Referral"] = inp
-        else:
-            # need to make a similarity checker
-            patient["Referral"] = patient["DerivedReferral"]
-
     save_json(referral_json, referral_db)
 
-    items_json = Path("itemNums.json")
-    items_db = load_json(items_json)
-    if collectionMode:
-        if "ProcNumbers" not in patient:
-            patient["ProcNumbers"] = list()
-            inp = input("Enter in numbers until all in, then enter y")
-            while inp != "y":
-                patient["ProcNumbers"].append(inp)
-                if inp not in items_db:
-                    items_db[inp] = inp
-        elif isinstance(patient["ProcNumbers"], list):
-            for i, number in enumerate(patient["ProcNumbers"]):
-                number = number.translate(str.maketrans("", "", string.punctuation))
-                number = re.sub(r"\D", "", number)
-                if number in items_db:
-                    if items_db[number] != number:
-                        inp = input(f"Is {items_db[number]} supposed to be instead of {number}? Enter y or enter the true number")
-                        if inp == "y":
-                            patient["ProcNumbers"][i] = items_db[number]
-                        else:
-                            patient["ProcNumbers"][i] = inp
-                    else:
-                        patient["ProcNumbers"][i] = number
-                else:
-                    inp = input(f"Enter 'y' if {number} is correct, if not enter true number")
-                    if inp == "y":
-                        items_db[number] = number
-                        patient["ProcNumbers"][i] = number
-                    else:
-                        patient["ProcNumbers"][i] = inp
-                        items_db[number] = inp
-        else:
-            number = patient["ProcNumbers"].translate(str.maketrans("", "", string.punctuation))
-            number = re.sub(r"\D", "", number)
-            if number in items_db:
-                if items_db[number] != number:
-                    inp = input(f"Is {items_db[number]} supposed to be instead of {number}? Enter y or enter the true number")
-                    if inp == "y":
-                        patient["ProcNumbers"] = items_db[number]
-                    else:
-                        patient["ProcNumbers"] = inp
-                else:
-                    patient["ProcNumbers"] = number
-            else:
-                inp = input(f"Enter 'y' if {number} is correct, if not enter true number")
-                if inp == "y":
-                    items_db[number] = number
-                    patient["ProcNumbers"] = number
-                else:
-                    patient["ProcNumbers"] = inp
-                    items_db[number] = inp
-    else:
-        if "ProcNumbers" not in patient:
-            patient["ProcNumbers"] = list()
-            inp = input("Enter in numbers until all in, then enter y")
-            while inp != "y":
-                patient["ProcNumbers"].append(inp)
-        elif isinstance(patient["ProcNumbers"], list):
-            for i, number in enumerate(patient["ProcNumbers"]):
-                number = number.translate(str.maketrans("", "", string.punctuation))
-                number = re.sub(r"\D", "", number)
-                if number in items_db:
-                    patient["ProcNumbers"][i] = items_db[number]
-                else:
-                    inp = input(f"Error: {number}, enter true number")
-                    patient["ProcNumbers"][i] = inp
-        else:
-            number = patient["ProcNumbers"].translate(str.maketrans("", "", string.punctuation))
-            number = re.sub(r"\D", "", number)
-            if number in items_db:
-                patient["ProcNumbers"] = items_db[number]
-            else:
-                inp = input(f"Error: {number}, enter true number")
-                patient["ProcNumbers"] = inp
-        
-        if "Times" in patient:
-            if "61109" not in patient["ProcNumbers"] and "55118" in patient["ProcNumbers"]:
-                inp = input("Is 61109 missing? y/n")
-                while inp != "y" and inp != "n":
-                    inp = input()
-                if inp == "y":
-                    patient["ProcNumbers"].append("61109")
-            elif "61109" in patient["ProcNumbers"] and "55118" not in patient["ProcNumbers"]:
-                inp = input("Is 55118 missing? y/n")
-                while inp != "y" and inp != "n":
-                    inp = input()
-                if inp == "y":
-                    patient["ProcNumbers"].append("55118")
-    save_json(items_json, items_db)
-    
-    if "Times" in patient:
-        if isinstance(patient["Times"], list) and len(patient["Times"]) == 2:
-            time_1 = "".join(ch for ch in patient["Times"][0] if ch.isdigit())
-            time_2 = "".join(ch for ch in patient["Times"][1] if ch.isdigit())
-            if len(time_1) < 3 or len(time_1) > 4 or len(time_1) < 4 and int(time_1[0]) < 8 or len(time_1) > 3 and int(time_1[1]) > 7:
-                inp = input(f"Is {time_1} correct? y/n")
-                while inp != "y" and inp != "n":
-                    inp = input()
-                if inp == "y":
-                    patient["Times"][0] = time_1
-                else:
-                    patient["Times"][0] = input("Put in correct Time")
-            else:
-                patient["Times"][0] = time_1
-            
-            if len(time_2) < 3 or len(time_2) > 4 or len(time_2) < 4 and int(time_2[0]) < 8 or len(time_2) > 3 and int(time_2[1]) > 7:
-                inp = input(f"Is {time_2} correct? y/n")
-                while inp != "y" and inp != "n":
-                    inp = input()
-                if inp == "y":
-                    patient["Times"][1] = time_2
-                else:
-                    patient["Times"][1] = input("Put in correct Time")
-            else:
-                patient["Times"][1] = time_2
+def ensureCorrectTimes(patient, buttons):
+    if "61109" not in patient["ProcNumbers"] and "55118" in patient["ProcNumbers"]:
+        inp = input("Is 61109 missing? y/n")
+        while inp != "y" and inp != "n":
+            inp = input()
+        if inp == "y":
+            patient["ProcNumbers"].append("61109")
+    elif "61109" in patient["ProcNumbers"] and "55118" not in patient["ProcNumbers"]:
+        inp = input("Is 55118 missing? y/n")
+        while inp != "y" and inp != "n":
+            inp = input()
+        if inp == "y":
+            patient["ProcNumbers"].append("55118")
 
+    if isinstance(patient["Times"], list) and len(patient["Times"]) == 2:
+        time_1 = "".join(ch for ch in patient["Times"][0] if ch.isdigit())
+        time_2 = "".join(ch for ch in patient["Times"][1] if ch.isdigit())
+        if len(time_1) < 3 or len(time_1) > 4 or len(time_1) < 4 and int(time_1[0]) < 8 or len(time_1) > 3 and int(time_1[1]) > 7:
+            inp = input(f"Is {time_1} correct? y/n")
+            while inp != "y" and inp != "n":
+                inp = input()
+            if inp == "y":
+                patient["Times"][0] = time_1
+            else:
+                patient["Times"][0] = input("Put in correct Time")
+        else:
+            patient["Times"][0] = time_1
+        
+        if len(time_2) < 3 or len(time_2) > 4 or len(time_2) < 4 and int(time_2[0]) < 8 or len(time_2) > 3 and int(time_2[1]) > 7:
+            inp = input(f"Is {time_2} correct? y/n")
+            while inp != "y" and inp != "n":
+                inp = input()
+            if inp == "y":
+                patient["Times"][1] = time_2
+            else:
+                patient["Times"][1] = input("Put in correct Time")
+        else:
+            patient["Times"][1] = time_2
+    
+
+def correctDetails(patient, buttons):
+    time.sleep(0.5)
+    if get_window_name() != "Windows PowerShell":
+        buttons["Terminal"].clickButton()
+    
+    ensurePatientFound(patient, buttons)
+    if not processPatientQuery(patient, buttons):
+        return 1
+    if not checkDOBandHospital(patient, buttons):
+        return 1
+    ensureCorrectDate(patient, buttons)
+    ensureCorrectReferral(patient, buttons)
+    ensureCorrectItemNumbers(patient, buttons)
+    if "Times" in patient:
+        ensureCorrectTimes(patient, buttons)
+    
     print(patient)
     inp = input("Patient ready, do you want to continue? y/n")
     if inp == "n":
